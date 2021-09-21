9e3991e0a59108510bdbbecf8ff8008e97ddd407
hugo.siqueira <hugo@agenciabee.com>
2021-09-21 09:38:23 -0300

    Alteração do Footer

--- themes/euphoriaadm/widgets/dash/footer.php
+++ themes/euphoriaadm/widgets/dash/footer.php
@@ -1,8 +1,8 @@
 <div class="footer-wrapper">
     <div class="footer-section f-section-1">
-        <p class="">Copyright © <?= date('Y');?> <a target="_blank" href="https://agenciabee.com">Hugo Siqueira</a>, Todos os direitos reservados.</p>
+        <p class="">Copyright © <?= date('Y');?> Euphoria-MG, Todos os direitos reservados.</p>
     </div>
     <div class="footer-section f-section-2">
-        <p class="">Desenvolvido com <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-heart"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg></p>
+        <p class="">Desenvolvido por <a target="_blank" href="https://agenciabee.com">Bee!</a></p>
     </div>
 </div>
